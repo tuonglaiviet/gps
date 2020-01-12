5439214a4c7525776558c60390ba80ed2e88af78
luu.hoait@gmail.com <luu.hoait@gmail.com>
2020-01-13 03:23:43 +0700

    contact backend

--- public/wp-content/themes/gwinnett-ps/tpl-locations.php
+++ public/wp-content/themes/gwinnett-ps/tpl-locations.php
@@ -13,69 +13,50 @@
 ?>
 <?php get_header() ?>
 <section class="gbanner top__banner__location__addcss">
-	<h2 class="top__banner__tit">Our Locations</h2>
+	<h2 class="top__banner__tit"><?php the_title(); ?></h2>
 </section>
+<?php 
+    $sub_title = get_field('sub_title');
+    $description = get_field('description');
+    if($sub_title || $description):
+?>
 <section class="contact__intro location-addstyle">
-    <h2 class="tit">Custom Care & Treatment.<br/>
-        Lasting Relationships.</h2>
+    <h2 class="tit"><?php echo $sub_title; ?></h2>
     <div class="txt">
-       <p>Gwinnett Pulmonary Group serves Gwinnett County and areas north of Atlanta with three convenient locations.</p>
+       <?php echo $description ?>
     </div>
 </section>
-
-<section class="location-items">
+<?php 
+    endif;
+?>
+<?php 
+    if( have_rows('locations') ):
+        $i = 0;
+         while ( have_rows('locations') ) : the_row(); $i++;
+            $link = get_sub_field('link');
+?>
+<section class="location-items <?php if($i%2) echo 'location-reverse' ?>">
     <div class="left">
         <div class="cover">
-            <h3 class="tit">Lawrenceville</h3>
+            <h3 class="tit"><?php get_sub_field('name'); ?></h3>
             <div class="txt">
-                Gwinnett Pulmonary Group - Lawrenceville<br>
-                631 Professional Drive (Gwinnett Medical Center)
-                Suite 350
-                Lawrenceville, GA 30046<br>
-                7:30 a.m. - 5 p.m.
+                <?php echo get_sub_field('content'); ?>
             </div>
-            <p class="btn-click"><a href="#">Visit</a></p>
+            <?php 
+                if($link):
+            ?>
+            <p class="btn-click"><a href="<?php echo $link ?>">Visit</a></p>
+            <?php 
+                endif;
+            ?>
         </div>
     </div>
     <div class="right">
         <img class="img" src="<?php bloginfo('template_directory') ?>/images/location-item1.png" alt=""/>
     </div>
 </section>
-
-<section class="location-items location-reverse">
-        <div class="left">
-            <div class="cover">
-                <h3 class="tit">Duluth</h3>
-                <div class="txt">
-                    Gwinnett Pulmonary Group – Duluth<br>
-                    Duluth Professional Center
-                    3855 Pleasant Hill Road
-                    Suite 180
-                    Duluth, GA 30096
-                    <br>
-                    7:30 a.m. - 5 p.m.
-                </div>
-                <p class="btn-click"><a href="#">Visit</a></p>
-            </div>
-        </div>
-        <div class="right">
-            <img class="img" src="<?php bloginfo('template_directory') ?>/images/location-item2.png" alt=""/>
-        </div>
-</section>
-
-<section class="location-items">
-        <div class="left">
-            <div class="cover">
-                <h3 class="tit">Hamilton Mill</h3>
-                <div class="txt">
-                    GMC Specialty Center 2108 Teron Trace Suite 100A Dacula, GA 30019
-                </div>
-                <p class="btn-click"><a href="#">Visit</a></p>
-            </div>
-        </div>
-        <div class="right">
-            <img class="img" src="<?php bloginfo('template_directory') ?>/images/location-item-3.png" alt=""/>
-        </div>
-</section>
-
+<?php 
+        endwhile;
+    endif;
+?>
 <?php get_footer() ?>
