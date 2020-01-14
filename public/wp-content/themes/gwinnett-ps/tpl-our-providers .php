4b6fa69bf144341a86146ddf4d9bc49eaee31ef9
hdttrinh <hdttrinh29@gmail.com>
2020-01-14 09:52:37 +0700

    providers

--- public/wp-content/themes/gwinnett-ps/tpl-our-providers .php
+++ public/wp-content/themes/gwinnett-ps/tpl-our-providers .php
@@ -50,7 +50,7 @@
             <div class="content">
                 <h3 class="title">LAWRENCE D. KAPLAN M.D.</h3>
                 <div class="description">
-                    Lawrence D. Kaplan M.D. is from Atlanta, Georgia. He received his undergraduate degree from Duke University in North Carolina, after which he completed medical school at Atlanta’s Emory University.
+                    <p>Lawrence D. Kaplan M.D. is from Atlanta, Georgia. He received his undergraduate degree from Duke University in North Carolina, after which he completed medical school at Atlanta’s Emory University.</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -72,7 +72,7 @@
             <div class="content">
                 <h3 class="title">MICHAEL J. SINEWAY M.D.</h3>
                 <div class="description">
-                    Michael J. Sineway M.D. is from Atlanta, Georgia. He completed his undergraduate studies at Georgia Institute of Technology, located in Atlanta, and then attended medical school at Vanderbilt University in Nashville, Tennessee.
+                    <p>Michael J. Sineway M.D. is from Atlanta, Georgia. He completed his undergraduate studies at Georgia Institute of Technology, located in Atlanta, and then attended medical school at Vanderbilt University in Nashville, Tennessee.</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -91,7 +91,7 @@
             <div class="content">
                 <h3 class="title">PAUL B. WEINBERG M.D.</h3>
                 <div class="description">
-                    Hailing from Atlanta, Georgia, Paul B. Weinberg M.D. attended High School at Westminster. After graduating he went to Brunswick, Maine where he obtained an undergraduate degree from Bowdoin College, majoring in History.
+                    <p>Hailing from Atlanta, Georgia, Paul B. Weinberg M.D. attended High School at Westminster. After graduating he went to Brunswick, Maine where he obtained an undergraduate degree from Bowdoin College, majoring in History.</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -110,7 +110,7 @@
             <div class="content">
                 <h3 class="title">WILLIAM F. McGann, JR. M.D.</h3>
                 <div class="description">
-                    Dr. McGann has been with Gwinnett Pulmonary and practicing Medicine in Gwinnett County since 1889. Dr. McGann was born in Peoria, Illinois. He received his undergraduate degree and medical degree from St. Louis University.
+                    <p>Dr. McGann has been with Gwinnett Pulmonary and practicing Medicine in Gwinnett County since 1889. Dr. McGann was born in Peoria, Illinois. He received his undergraduate degree and medical degree from St. Louis University.</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -130,7 +130,7 @@
             <div class="content">
                 <h3 class="title">PRASAD S. GARIMELLA M.D.</h3>
                 <div class="description">
-                    Dr. Garimella has been a Pulmonary, Critical Care and Sleep Medicine physician in private practice for over 8 years. He has a special interest and is well recognized in the evaluation and management of Sleep Disorders and Lung Cancer Diagnosis and Treatment.
+                    <p>Dr. Garimella has been a Pulmonary, Critical Care and Sleep Medicine physician in private practice for over 8 years. He has a special interest and is well recognized in the evaluation and management of Sleep Disorders and Lung Cancer Diagnosis and Treatment.</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -150,7 +150,7 @@
             <div class="content">
                 <h3 class="title">RAJESH R. JASANI M.D.</h3>
                 <div class="description">
-                    Dr. Jasani has been a pulmonary, critical care and sleep medicine physician in Gwinnett for over 10 years. He has a special interest and is well recognized in the evaluationand management of sleep disorders.
+                    <p>Dr. Jasani has been a pulmonary, critical care and sleep medicine physician in Gwinnett for over 10 years. He has a special interest and is well recognized in the evaluationand management of sleep disorders.</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -170,7 +170,7 @@
             <div class="content">
                 <h3 class="title">SARAH J. HAYAT M.D.</h3>
                 <div class="description">
-                    Sarah J. Hayat M.D. received her undergraduate degree from St. Joseph’s College for Women in Pakistan. She completed medical school at AGA Khan University Medical College, also in Pakistan.
+                    <p>Sarah J. Hayat M.D. received her undergraduate degree from St. Joseph’s College for Women in Pakistan. She completed medical school at AGA Khan University Medical College, also in Pakistan.</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -189,7 +189,7 @@
             <div class="content">
                 <h3 class="title">VOSUDESH K. PAI M.D.</h3>
                 <div class="description">
-                    Dr. Vosudesh Pai is from Hyderabad, India where he received a Doctorate in Medicine. He completed his residency in Internal Medicine and fellowship in Pulmonary, Critical Care and Sleep Medicine from Henry Ford Hospital.
+                    <p>Dr. Vosudesh Pai is from Hyderabad, India where he received a Doctorate in Medicine. He completed his residency in Internal Medicine and fellowship in Pulmonary, Critical Care and Sleep Medicine from Henry Ford Hospital.</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -208,7 +208,7 @@
             <div class="content">
                 <h3 class="title">Waqar Siddiqui, M.D.</h3>
                 <div class="description">
-                    Dr. Susmita Rajanala is from Hyderabad, India, where she received a Doctorate in Medicine. She completed her residency in Internal Medicine from Seton Hall University, New Jersey and a Critical Care Medicine fellowship from
+                    <p>Dr. Susmita Rajanala is from Hyderabad, India, where she received a Doctorate in Medicine. She completed her residency in Internal Medicine from Seton Hall University, New Jersey and a Critical Care Medicine fellowship from</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
@@ -227,12 +227,12 @@
             <div class="content">
                 <h3 class="title">Lynn Jackman, MSN, NP-C, CCRN-CSC</h3>
                 <div class="description">
-                    Lynn joined Gwinnett Pulmonary Group in July 2014. Originally from Indiana, she began her nursing career at Indiana University Medical Center. She comes to Gwinnett Pulmonary Group with over 20 years of vast nursing
+                    <p>Lynn joined Gwinnett Pulmonary Group in July 2014. Originally from Indiana, she began her nursing career at Indiana University Medical Center. She comes to Gwinnett Pulmonary Group with over 20 years of vast nursing</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
                     <div class="ac__content">
-                        Lynn joined Gwinnett Pulmonary Group in July 2014. Originally from Indiana, she began her nursing career at Indiana University Medical Center. She comes to Gwinnett Pulmonary Group with over 20 years of vast nursing experience in Cardiovascular Critical Care and Critical Care. Lynn is highly regarded as a leader and expert in the nursing field.
+                        <p>Lynn joined Gwinnett Pulmonary Group in July 2014. Originally from Indiana, she began her nursing career at Indiana University Medical Center. She comes to Gwinnett Pulmonary Group with over 20 years of vast nursing experience in Cardiovascular Critical Care and Critical Care. Lynn is highly regarded as a leader and expert in the nursing field.</p>
                     </div>
                     <p class="gbutton2 js_click_show">Close</p>
                 </div>
@@ -246,12 +246,12 @@
             <div class="content">
                 <h3 class="title">Opal M. Blake, MS, APRN, AGACNP-BC, NP-C</h3>
                 <div class="description">
-                    Opal joined Gwinnett Pulmonary Group in September 2014 as a Nurse Practitioner. She is Double Certified as an Adult Gerontology Acute Care Nurse Practitioner and an Adult Gerontology Primary Care Nurse Practitioner. She
+                    <p>Opal joined Gwinnett Pulmonary Group in September 2014 as a Nurse Practitioner. She is Double Certified as an Adult Gerontology Acute Care Nurse Practitioner and an Adult Gerontology Primary Care Nurse Practitioner. She</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
                     <div class="ac__content">
-                        Opal joined Gwinnett Pulmonary Group in September 2014 as a Nurse Practitioner. She is Double Certified as an Adult Gerontology Acute Care Nurse Practitioner and an Adult Gerontology Primary Care Nurse Practitioner. She received her Post-Masters Acute Care Certification at Emory University and her Master of Science in Nursing with a focus in Adult Gerontology Primary Care and Clinical Nurse Specialist from Georgia State University. Prior to joining us, she worked at Gwinn
+                        <p>Opal joined Gwinnett Pulmonary Group in September 2014 as a Nurse Practitioner. She is Double Certified as an Adult Gerontology Acute Care Nurse Practitioner and an Adult Gerontology Primary Care Nurse Practitioner. She received her Post-Masters Acute Care Certification at Emory University and her Master of Science in Nursing with a focus in Adult Gerontology Primary Care and Clinical Nurse Specialist from Georgia State University. Prior to joining us, she worked at Gwinn</p>
                     </div>
                     <p class="gbutton2 js_click_show">Close</p>
                 </div>
@@ -265,12 +265,12 @@
             <div class="content">
                 <h3 class="title">Antonette Sanderson, MS, NP-C</h3>
                 <div class="description">
-                    Lynn joined Gwinnett Pulmonary Group in July 2014. Originally from Indiana, she began her nursing career at Indiana University Medical Center. She comes to Gwinnett Pulmonary Group with over 20 years of vast nursing
+                    <p>Lynn joined Gwinnett Pulmonary Group in July 2014. Originally from Indiana, she began her nursing career at Indiana University Medical Center. She comes to Gwinnett Pulmonary Group with over 20 years of vast nursing</p>
                 </div>
                 <p class="js_more gbutton2 about__stories__box--button">Read more</p>
                 <div class="active__post js_less scp-programs-active">
                     <div class="ac__content">
-                        Lynn joined Gwinnett Pulmonary Group in July 2014. Originally from Indiana, she began her nursing career at Indiana University Medical Center. She comes to Gwinnett Pulmonary Group with over 20 years of vast nursing experience in Cardiovascular Critical Care and Critical Care. Lynn is highly regarded as a leader and expert in the nursing field.
+                        <p>Lynn joined Gwinnett Pulmonary Group in July 2014. Originally from Indiana, she began her nursing career at Indiana University Medical Center. She comes to Gwinnett Pulmonary Group with over 20 years of vast nursing experience in Cardiovascular Critical Care and Critical Care. Lynn is highly regarded as a leader and expert in the nursing field.</p>
                     </div>
                     <p class="gbutton2 js_click_show">Close</p>
                 </div>
